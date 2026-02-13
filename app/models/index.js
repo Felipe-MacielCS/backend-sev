98d9a37fea99ada313d1a5f885c40eb093a4d974
carlen.gattenby <carlen.gattenby@eagles.oc.edu>
2026-02-13 13:00:42 -0600

    notification back end

--- app/models/index.js
+++ app/models/index.js
@@ -54,6 +54,12 @@ db.result.belongsTo(db.goal, { foreignKey: "goalID" });
 db.coach.hasMany(db.exerciseplan, { foreignKey: "coachID", onDelete: "CASCADE" });
 db.exerciseplan.belongsTo(db.coach, { foreignKey: "coachID" });
 
+db.notification.hasMany(db.usernotification, { foreignKey: "notificationID" });
+db.usernotification.belongsTo(db.notification, { foreignKey: "notificationID" });
+
+db.user.hasMany(db.usernotification, { foreignKey: "userID" });
+db.usernotification.belongsTo(db.user, { foreignKey: "userID" });
+
 // Exercise to ExercisePlan using exercisePool
 db.exercise.belongsToMany(db.exerciseplan, {
   through: db.exercisepool,
